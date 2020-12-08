94d77a5402796e347ed827a0875baa165fdde526
Rajahni fields <rajahnifields@gmail.com>
2020-12-08 00:44:30 -0500

    git commit -a

--- calendar.js
+++ calendar.js
@@ -2,11 +2,19 @@
 $("#currentDay").text(luxon.DateTime.local().toLocaleString({
      weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }));
 
-    // 'MMMM Do YYYY, h:mm:ss a'  
-
-
-// add dates and times to the calendar 
 //  Make the calendar editable
+function timetravel(){
+let hourPlan = luxon.DateTime.local().toLocaleString({hour: 2-digit})
+}
+
+let currentHour =parseInt(hourPlan[0] + hourPlan[1])
+if (hourPlan.indexof("am")){
+    currentHour -=12
+    console.log(currentHour)
+}
+
+
+
 // able to save the events on calendar
 // able to add different colors depend on pass events
 
