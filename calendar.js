e29a905f70d47e6a201712a005764f6f413d0664
Rajahni fields <rajahnifields@gmail.com>
2020-12-08 00:25:21 -0500

    git commit .

---
+++ calendar.js
@@ -0,0 +1,13 @@
+// interactive day planner
+$("#currentDay").text(luxon.DateTime.local().toLocaleString({
+     weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }));
+
+    // 'MMMM Do YYYY, h:mm:ss a'  
+
+
+// add dates and times to the calendar 
+//  Make the calendar editable
+// able to save the events on calendar
+// able to add different colors depend on pass events
+
+
