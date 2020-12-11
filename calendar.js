570392db90707ac46bc2443d66f5d1fa0ffc20c8
Rajahni fields <rajahnifields@gmail.com>
2020-12-10 23:37:26 -0500

    git commit .

--- calendar.js
+++ calendar.js
@@ -4,63 +4,53 @@
      weekday: 'short', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' }));
 
 //  Make the calendar editable
-    function timetravel(){
-    let hourPlan = luxon.DateTime.local().toLocaleString({hour:2-digit})
+    
+    let hourPlan = luxon.DateTime.local().toLocaleString({hour:'2-digit'})
 
-    let currentHour =parseInt (hourPlan[0] + hourPlan[1])
+    let currentHour =parseInt(hourPlan[0] + hourPlan[1])
 
-    if (hourPlan.indexof("am")!== +1){
-    currentHour -=12
-    // console.log(currentHour)
+    if (hourPlan.indexOf("am")!== +1){
+    currentHour +=12
+    console.log(currentHour)
     }
 
     // able to add different colors depend on pass events
-    }
+    
 
-    $("text").each(function(){
-    let timeNow=parseInt($(this).attr("id"))
-
+    $(".text").each(function(){
+    let timeNow=parseInt($(this).parent().attr("id"))
+    console.log(timeNow)
+        $(this).val(localStorage.getItem(timeNow));
         if(currentHour < timeNow){
-        $(this).addClass("#d3d3d3")
+        $(this).parent().addClass("past")
         // $row.css ("background-color","#d3d3d3")
     
         console.log=("past wow it looks so different")
         }
-        else if (present === timeNow){
-        $(this).addClass("#ff6961")
+        else if (currentHour === timeNow){
+        $(this).parent().addClass("present")
         // $row.css ("background-color","#ff6961")
         }
-        else {(present > timeNow)
-        $(this).addClass("#77dd77")
+        else {(currentHour > timeNow)
+        $(this).parent().addClass("future")
         // $row.css ("background-color","#77dd77")
         }
 
     })
            
 // save message
-           
-           $( ".saveBtn" ).click(function() {
+           var saveBtn = $(".saveBtn")
+           $(".saveBtn").on ("click",function() {
            console.log("yay it works")
-            //setting items in the local storage
-                localStorage.setItem("9", $9am),
-                localStorage.setItem("10", $10am),
-                localStorage.setItem("11", $11am),
-                localStorage.setItem("12", $12pm),
-                localStorage.setItem("13", $1pm),
-                localStorage.setItem("14", $2pm),
-                localStorage.setItem("15", $3pm),
-                localStorage.setItem("16", $4pm),
-                localStorage.setItem("17", $5pm),
-           }),    
+
+           let timeBlock = $(this).parent().attr("id");
            
-                $("#text9AM").append(localStorage.getItem("9")),
-                $("#text10AM").append(localStorage.getItem("10")),
-                $("#text11AM").append(localStorage.getItem("11")),
-                $("#text12AM").append(localStorage.getItem("12")),
-                $("#text1PM").append(localStorage.getItem("13")),
-                $("#text2PM").append(localStorage.getItem("14")),
-                $("#text3PM").append(localStorage.getItem("15")),
-                $("#text4PM").append(localStorage.getItem("16")),
-                $("#text5PM").append(localStorage.getItem("17")),
-        )
+           let input= $(this).prev().val();
+           localStorage.setItem(timeBlock, input)
+            console.log(input)
+           
+           })     
+           
+     
+        
            
